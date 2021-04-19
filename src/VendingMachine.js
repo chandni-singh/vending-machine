b687596e8a969f753bb91fac698863b7eae35939
Chandni <chandni1141996@gmail.com>
2021-04-19 22:04:28 +0530

    starter records

---
+++ src/VendingMachine.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class VendingMachine extends Component {
+    render() {
+        return (
+            <h1>Vending Machine</h1>
+        )
+    }
+}
+
+export default VendingMachine;
